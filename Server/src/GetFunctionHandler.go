af80509876d5c4f5dcfd0354488fb2484d59ddc6
Lokloll <niclas.allgaeuer@outlook.com>
2022-10-20 08:45:21 +0200

    Uploading Basic fiber package and server respone to root and /test

---
+++ Server/src/GetFunctionHandler.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func GetBasicDebug(ctx *fiber.Ctx) error {
+	return ctx.SendString("response")
+}
---
+++ Server/src/Main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", GetBasicDebug)
+	app.Get("/test", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("FakeResponse")
+	})
+
+	app.Listen(":443")
+}
