6597aff9763f5233c0343c8977abf4cb4beb7ef2
Lokloll <niclas.allgaeuer@outlook.com>
2022-10-20 09:24:17 +0200

    Uploading HTTPRequest.hpp

---
+++ Client/src/cmake-build-debug-remote/CMakeFiles/C2CLIClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "C2CLIClient"
+  "C2CLIClient.pdb"
+  "CMakeFiles/C2CLIClient.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C2CLIClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
