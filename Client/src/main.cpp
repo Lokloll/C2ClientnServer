77db36476e1013db56aeee12e33c6b3825c74cc6
Lokloll <niclas.allgaeuer@outlook.com>
2022-10-20 09:23:18 +0200

    Uploading HTTPRequest.hpp

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -2,8 +2,23 @@
 // Created by nicla on 20.10.2022.
 //
 #include <iostream>
+#include "HTTPRequest.hpp"
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    try
+    {
+        http::Request request{"http://test.com/test"};
+        const string body = "foo=1&bar=baz";
+        const auto response = request.send("POST", body, {
+                {"Content-Type", "application/x-www-form-urlencoded"}
+        });
+        std::cout << std::string{response.body.begin(), response.body.end()} << '\n'; // print the result
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Request failed, error: " << e.what() << '\n';
+    }
     return 0;
 }
